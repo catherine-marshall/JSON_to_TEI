a056cea6bfd4b98d054af44ff5f2afdbc871c680
catherine-marshall <79547575+catherine-marshall@users.noreply.github.com>
2023-05-20 20:13:18 -0600

    Added Bitext and source and target text

--- JSON_to_TEI.py
+++ JSON_to_TEI.py
@@ -3,52 +3,66 @@ import json
 
 filename = "Korean_test_report.json"
 
+# This opens and retrieves the text from the JSON file
 with open(filename, 'r', encoding='utf-8') as json_file:
     json_load = json.load(json_file)
     # print(json_load)
 
-project_name = json_load["projectName"]
+# These are all the input questions
+publisher = input("What is the publisher's name? ")
+source_desc = input("Provide a brief description for this document: ")
+source_lang = input("What is the language code of the source language? ")
+target_lang = input("What is the language code of the target language? ")
 
-publisher = input("What is the publisher's name?")
-source_desc = input("Provide a brief description for this document:")
-
+# This creates the output file
 filename = "korean-test-tei.xml"
 file = open(filename, "w", encoding="utf-8")
 file.write("")
 file.close()
 
-print('<TEI xmlns="http://www.tei-c.org/ns/1.0">'
-      '  <teiHeader xml:lang="en">'
-      '\t<fileDesc>'
-      '\t\t<titleStmt>'
-      '\t\t\t<title>' + project_name + '</title>'
-      '\t\t</titleStmt>'
-      '\t\t<publicationStmt>'
-      '\t\t\t<publisher>' + publisher + '</publisher>'
-      '\t\t</publicationStmt>'
-      '\t\t<sourceDesc>'
-      '\t\t\t<p>' + source_desc + '</p>'
-      '\t\t</sourceDesc>'
-      '\t</fileDesc>'
-      '  </teiHeader>'
-      '  <text>'
-      '   <body>'
-      '\t<list>',
-      file=open(filename,"a", encoding="utf-8"))
+# This prints the header
+project_name = json_load["projectName"]
+tei_header = """<TEI xmlns="http://www.tei-c.org/ns/1.0">
+  <teiHeader xml:lang="en">
+  	<fileDesc>
+  		<titleStmt>
+  			<title>""" + project_name + """</title>
+  		</titleStmt>
+  		<publicationStmt>
+  			<publisher>""" + publisher + """</publisher>
+  		</publicationStmt>
+  		<sourceDesc>
+  			<p>""" + source_desc + """</p>
+  		</sourceDesc>
+  	</fileDesc>
+  </teiHeader>
+  <text>
+   <body>
+	<list>"""
 
+print(tei_header, file=open(filename, "a", encoding="utf-8"))
+
+
+# This prints the key
 key = json_load["key"]
 
+seg_list = []
 for k in key:
     key_value = '\t\t<item n="' + k + '">' + key[k] + '</item>'
     print(key_value,file=open(filename, "a", encoding="utf-8"))
+    seg_list.append(key[k])
 
 errors = json_load["errors"]
 
-print('\t</list>'
+print('\t</list>\n'
       '\t<div type="errors">',
       file=open(filename,"a", encoding="utf-8"))
 
+
+# This prints the error info
 for error in errors:
+    if error["note"][-1] == '\n':
+          error["note"] = error["note"][:-1]
     print('\t <div type="error">\n'
           '\t  <list>\n'
           '\t\t<item n="segment">' + error["segment"] + '</item>\n'
@@ -63,6 +77,8 @@ for error in errors:
           '\t </div>',
           file=open(filename, "a", encoding="utf-8"))
 
+
+# This prints the metric
 metrics = json_load["metric"]
 
 print('\t</div>\n'
@@ -85,3 +101,51 @@ for metric in metrics:
 print('\t</div>',
       file=open(filename, "a", encoding="utf-8"))
 
+
+# This prints the composite score
+score = json_load["scores"]
+
+for k in score:
+      comp_score = score[k]
+
+print('\t<div type="scores">\n'
+      '\t <list>\n'
+      '\t \t<item n="compositeScore">' + comp_score + '</item>\n'
+      '\t </list>\n'
+      '\t</div>',
+      file=open(filename, "a", encoding="utf-8"))
+
+
+# This prints the bitext linkGrp
+print('\t<linkGrp type="translation">', file=open(filename, "a", encoding="utf-8"))
+for seg in seg_list:
+      print('\t\t<link target =" #x' + seg + ' #y' + seg + ' "/>', file=open(filename, "a", encoding="utf-8"))
+print('\t</linkGrp>', file=open(filename, "a", encoding="utf-8"))
+
+
+# This prints the source and target texts
+source_text = json_load["segments"]["source"]
+target_text = json_load["segments"]["target"]
+
+print('\t<div type="volume" xml:id="x" xml:lang ="' + source_lang + '">\n'
+      '\t  <p>', file=open(filename, "a", encoding="utf-8"))
+for seg in seg_list:
+      if source_text[int(seg) - 1][-1] == "\n" or source_text[int(seg) - 1][-1] == "\r":
+            source_text[int(seg) - 1] = source_text[int(seg) - 1][:-1]
+      print('\t\t<s xml:id="x' + seg + '">' + source_text[int(seg) - 1] + '</s>', file=open(filename, "a", encoding="utf-8"))
+print('\t  </p>\n'
+      '\t</div>', file=open(filename, "a", encoding="utf-8"))
+
+print('\t<div type="volume" xml:id="y" xml:lang ="' + target_lang + '">\n'
+      '\t  <p>', file=open(filename, "a", encoding="utf-8"))
+for seg in seg_list:
+      if target_text[int(seg) - 1][-1] == "\n" or target_text[int(seg) - 1][-1] == "\r":
+            target_text[int(seg) - 1] = target_text[int(seg) - 1][:-1]
+      print('\t\t<s xml:id="x' + seg + '">' + target_text[int(seg) - 1] + '</s>', file=open(filename, "a", encoding="utf-8"))
+print('\t  </p>\n'
+      '\t</div>', file=open(filename, "a", encoding="utf-8"))
+
+# This prints the closing tags at the end
+print('   </body>\n'
+      '  </text>\n'
+      '</TEI>', file=open(filename, "a", encoding="utf-8"))
